356144f4991b8d13d212f9375d001baaaa1b2c6c
semwai <33517045+semwai@users.noreply.github.com>
2019-12-23 21:54:16 +0300

    231219

--- app/src/main/java/com/semwai/haffman/MainActivity.kt
+++ app/src/main/java/com/semwai/haffman/MainActivity.kt
@@ -1,36 +1,50 @@
 package com.semwai.haffman
 
-import android.content.Context
-import android.graphics.*
+
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.View
-import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
-
-import android.R.attr.y
-import android.R.attr.x
 import android.content.Intent
-
-import android.graphics.Paint.ANTI_ALIAS_FLAG
-
-
+import android.widget.Button
 
 
 class MainActivity : AppCompatActivity() {
 
     val myMap = mutableMapOf<Char,Int>()
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        generator.setOnClickListener {
+
+
+            list.removeAllViews()
+            myMap.clear()
+            val chars = 'a'..'z'
+            val numbers = 1..20
+            for (i in  0..20){
+                val l = chars.shuffled().first()
+                val c = numbers.shuffled().first()
+                myMap[l] = c
+                val t = Button(this)
+                t.text = "${c} - ${l}"
+                t.setOnClickListener {
+                        btn ->
+                    myMap.remove(l)
+                    list.removeView(btn)
+
+
+                }
+                list.addView(t)
+            }
+        }
         button2.setOnClickListener {
             val letter = inputChar.text.firstOrNull()
             if (letter != null){
                 myMap[letter] = myMap.getOrDefault(letter,0) + (count.text.toString().toIntOrNull()?:0)
                 list.removeAllViews()
                 myMap.toList().reversed().forEach {
-                    val t = TextView(this)
+                    val t = Button(this)
                     t.text = "${it.first} - ${it.second}"
                     t.setOnClickListener {
                             btn ->
@@ -44,6 +58,7 @@ class MainActivity : AppCompatActivity() {
         }
         drawButton.setOnClickListener {
             val i = Intent(this,drawer::class.java)
+
             i.putExtra("map", myMap.toString())
             startActivity(i)
         }
--- app/src/main/java/com/semwai/haffman/drawer.kt
+++ app/src/main/java/com/semwai/haffman/drawer.kt
@@ -14,6 +14,7 @@ import android.support.v4.content.ContextCompat.getSystemService
 import android.view.Display
 import android.R.attr.y
 import android.R.attr.x
+import android.support.annotation.ColorInt
 import android.util.Log
 
 
@@ -39,63 +40,82 @@ class drawer : AppCompatActivity() {
 internal class DrawView(context: Context) : View(context) {
 
     var p: Paint
-    var path: Path
-    var point1: Point
-    var point21: Point
-    var point22: Point
+    var t: Paint
+    var nodeRadius = 0F
+    var canvas: Canvas? = null
 
     init {
         p = Paint(Paint.ANTI_ALIAS_FLAG)
-        p.setStrokeWidth(3F)
-        path = Path()
+        p.color = Color.GREEN
+        t = Paint(Paint.ANTI_ALIAS_FLAG)
+        t.strokeWidth = 3F
+        t.color = Color.BLACK
 
-        point1 = Point(200, 300)
-        point21 = Point(500, 600)
-        point22 = Point(900, 200)
     }
 
-    override fun onDraw(canvas: Canvas) {
-        canvas.drawARGB(80, 102, 204, 255)
-
-
+    override fun onDraw(cnv: Canvas) {
+        canvas = cnv
+        canvas!!.drawARGB(80, 102, 204, 255)
+        /*
         // первая линия
         p.color = Color.BLACK
         canvas.drawLine(100F, 100F, 600F, 100F, p)
-
         // точка отклонения для первой линии
         p.style = Paint.Style.FILL
         p.color = Color.GREEN
         canvas.drawCircle(point1.x.toFloat(), point1.y.toFloat(), 10F, p)
-
         // квадратичная кривая
         path.reset()
         path.moveTo(100F, 100F)
         path.quadTo(point1.x.toFloat(), point1.y.toFloat(), 600F, 100F)
         p.style = Paint.Style.STROKE
         canvas.drawPath(path, p)
-
-
         // вторая линия
         p.color = Color.BLACK
         canvas.drawLine(400F, 400F, 1100F, 400F, p)
-
         // точки отклонения для второй линии
         p.style = Paint.Style.FILL
         p.color = Color.BLUE
         canvas.drawCircle(point21.x.toFloat(), point21.y.toFloat(), 10F, p)
         canvas.drawCircle(point22.x.toFloat(), point22.y.toFloat(), 10F, p)
-
         // кубическая кривая
         path.reset()
         path.moveTo(400F, 400F)
         path.cubicTo(point21.x.toFloat(), point21.y.toFloat(), point22.x.toFloat(), point22.y.toFloat(), 1100F, 400F)
         p.style = Paint.Style.STROKE
         canvas.drawPath(path, p)
-
-
-
         p.textSize = 30f
         for (i in 0..100)
         canvas.drawText("hello", 10f, (i*40).toFloat(),p)
+        canvas.drawText("width ${canvas.width} height ${canvas.height}", 40f, 40f,p)*/
+        nodeRadius = (cnv.width + cnv.height) / 35F
+        t.textSize = (cnv.width + cnv.height) / 70F
+
+        drawNode("abcde", 15, false, 100F, 200F)
+        drawNode("ac", 10, true, 150F, 330F)
+        drawEdge(100F,200F,150F,330F)
+    }
+
+    /*
+    рисует отдельную вершину
+     */
+    fun drawNode(text: String, weight: Int, point: Boolean, x: Float, y: Float) {
+        p.color = Color.BLACK
+        canvas!!.drawCircle(x, y, nodeRadius + 2, p)
+        p.color = Color.parseColor("#008577")
+        canvas!!.drawCircle(x, y, nodeRadius, p)
+        canvas!!.drawText(text, x - t.textSize * text.length / 4, y + 4, t)
+        canvas!!.drawText(weight.toString(), x + nodeRadius + 2, y + 4, t)
+        canvas!!.drawText(if (point) "1" else "0", x - nodeRadius - 15, y + 4, t)
+    }
+
+    fun drawEdge(x1: Float, y1: Float, x2: Float, y2: Float) {
+        val angle = Math.atan2(y2.toDouble() - y1.toDouble(), x2.toDouble() - x1.toDouble())
+        canvas!!.drawLine(
+            x1 + nodeRadius*Math.cos(angle).toFloat(),
+            y1 + nodeRadius*Math.sin(angle).toFloat(),
+            x2 - nodeRadius*Math.cos(angle).toFloat(),
+            y2 - nodeRadius*Math.sin(angle).toFloat(),
+            t)
     }
 }

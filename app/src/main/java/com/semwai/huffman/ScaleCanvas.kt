0549b6500d31f9941e43aec12be4472ac07151b6
semwai <33517045+semwai@users.noreply.github.com>
2020-05-28 20:12:32 +0300

    fix https://github.com/semwai/Haffman/issues/1#issuecomment-633297340

--- app/src/main/java/com/semwai/huffman/ScaleCanvas.kt
+++ app/src/main/java/com/semwai/huffman/ScaleCanvas.kt
@@ -5,9 +5,6 @@ import android.graphics.Canvas
 import android.view.MotionEvent
 import android.view.ScaleGestureDetector
 import android.view.View
-import kotlin.concurrent.thread
-
-
 
 open class ScaleCanvas(context: Context) : View(context) {
 
@@ -24,33 +21,24 @@ open class ScaleCanvas(context: Context) : View(context) {
 
     private var startX = mutableMapOf<Int, Float>()
     private var startY = mutableMapOf<Int, Float>()
-    private var touchCounter = 0
-    override fun onTouchEvent(event: MotionEvent?): Boolean {
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
         mScaleDetector.onTouchEvent(event)
-
-        val fingerIndex = event?.actionIndex ?: 0
-        val fingerId = event?.getPointerId(fingerIndex) ?: 0
-
-        if (!isScaleProcess)
-            when (event?.actionMasked) {
-                MotionEvent.ACTION_MOVE -> {
-                    val egx = event.getX(fingerIndex)
-                    val egy = event.getY(fingerIndex)
-                    xOffset += (startX[fingerId]?:egx) - egx
-                    yOffset += (startY[fingerId]?:egy) - egy
-                    startX[fingerId] = egx
-                    startY[fingerId] = egy
-                }
-                MotionEvent.ACTION_DOWN, MotionEvent.ACTION_POINTER_DOWN -> {
-                    touchCounter++
-                    startX[fingerId] = event.getX(fingerIndex)
-                    startY[fingerId] = event.getY(fingerIndex)
-                }
-                MotionEvent.ACTION_UP, MotionEvent.ACTION_POINTER_UP -> {
-                    touchCounter--
-                }
+        val fingerId = event.getPointerId(0)
+        when (event.actionMasked) {
+            MotionEvent.ACTION_MOVE -> {
+                val egx = event.x
+                val egy = event.y
+                xOffset += (startX[fingerId] ?: egx) - egx
+                yOffset += (startY[fingerId] ?: egy) - egy
+                startX[fingerId] = egx
+                startY[fingerId] = egy
             }
-
+        }
+        for (i in 0 until event.pointerCount) {
+            startX[i] = event.getX(i)
+            startY[i] = event.getY(i)
+        }
         //перерисовка после сдвигов
         invalidate()
         //если иначе, то не работает move
@@ -58,14 +46,12 @@ open class ScaleCanvas(context: Context) : View(context) {
     }
 
     private val scaleListener = object : ScaleGestureDetector.SimpleOnScaleGestureListener() {
-
         val minScale = 0.1f
         val maxScale = 4.0f
         override fun onScale(detector: ScaleGestureDetector): Boolean {
             val newScale = scale * detector.scaleFactor
             if (newScale in minScale..maxScale) {
                 scale = newScale
-                //мне кажется я решил проблему не совсем как вы предложили, но это вполне работает
                 xOffset =
                     xOffset * detector.scaleFactor + detector.focusX * (detector.scaleFactor - 1)
                 yOffset =
@@ -73,23 +59,7 @@ open class ScaleCanvas(context: Context) : View(context) {
             }
             return true
         }
-
-        override fun onScaleBegin(detector: ScaleGestureDetector?): Boolean {
-            isScaleProcess = true
-            return super.onScaleBegin(detector)
-        }
-
-        override fun onScaleEnd(detector: ScaleGestureDetector?) {
-            super.onScaleEnd(detector)
-            //позволяет убрать лишнее поддергивание после окончания скейлинга.
-            thread {
-                Thread.sleep(50)
-                isScaleProcess = false
-            }
-        }
     }
     private val mScaleDetector = ScaleGestureDetector(context, scaleListener)
-    private var isScaleProcess =
-        false // при скалировании будем блокировать другое перемещение сцены
 }
 
